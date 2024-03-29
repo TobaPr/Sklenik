49de8c3d0274fd509d654b4618f8f91ec01c6956
ToPr <prajza.t@gmail.com>
2024-03-29 16:15:58 +0100

    Update test_casu.py

--- test_casu.py
+++ test_casu.py
@@ -5,8 +5,8 @@ import adafruit_ds3231
 
 
 # Definice GPIO pinů pro I2C komunikaci
-scl_pin = 3
-sda_pin = 2
+sda_pin = board.GPIO27  # GPIO27 pro SDA
+scl_pin = board.GPIO22  # GPIO22 pro SCL
 
 # Inicializace I2C sběrnice s definovanými piny
 i2c = busio.I2C(scl=scl_pin, sda=sda_pin)
