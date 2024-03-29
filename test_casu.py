c5cc4168dfcfcc25c44ced83de82742a04383cb0
ToPr <prajza.t@gmail.com>
2024-03-29 16:05:34 +0100

    Update test_casu.py

--- test_casu.py
+++ test_casu.py
@@ -2,8 +2,14 @@ import board
 import busio
 import adafruit_ds3231
 
-# Inicializace I2C sběrnice
-i2c = busio.I2C(2, 3)
+
+
+# Definice GPIO pinů pro I2C komunikaci
+scl_pin = board.SCL_1  # Pin pro SCL I2C (např. GPIO3)
+sda_pin = board.SDA_1  # Pin pro SDA I2C (např. GPIO2)
+
+# Inicializace I2C sběrnice s definovanými piny
+i2c = busio.I2C(scl=scl_pin, sda=sda_pin)
 
 
 # Inicializace objektu RTC
