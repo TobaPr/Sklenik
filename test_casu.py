c8a446da61109a56c82cce8b90ef7e2d49423e8e
ToPr <prajza.t@gmail.com>
2024-03-29 18:20:39 +0100

    Update test_casu.py

--- test_casu.py
+++ test_casu.py
@@ -12,13 +12,6 @@ rtc = adafruit_ds3231.DS3231(i2c)
 current_time = rtc.datetime
 
 
-# Nastavení času RTC na aktuální čas (už není potřeba)
-# Získání aktuálního času
-# now = time.localtime()
-# rtc.datetime = now
-# print("Čas na RTC modulu byl nastaven na aktuální čas:", now)
-
-
 print("Aktuální čas z RTC:")
 print("Rok: {:04d}".format(current_time.tm_year))
 print("Měsíc: {:02d}".format(current_time.tm_mon))
