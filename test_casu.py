22f295de6013e379064f8845be370b4c759a1312
ToPr <prajza.t@gmail.com>
2024-03-29 16:40:38 +0100

    Update test_casu.py

--- test_casu.py
+++ test_casu.py
@@ -1,13 +1,8 @@
-import board
-import busio
 import adafruit_ds3231
+import time
+import board
 
-
-
-
-
-# Inicializace I2C sběrnice s definovanými piny
-i2c = busio.I2C(scl=15, sda=13)
+i2c = board.I2C()  # uses board.SCL and board.SDA
 
 
 # Inicializace objektu RTC
