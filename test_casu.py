1c6bb78fe0ff927472e25e24286add0c81423feb
ToPr <prajza.t@gmail.com>
2024-03-29 15:25:20 +0100

    Create test_casu.py
    Test hodin reálného času.

---
+++ test_casu.py
@@ -0,0 +1,14 @@
+import board
+import busio
+import adafruit_ds3231
+
+# Inicializace I2C sběrnice
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Inicializace objektu RTC
+rtc = adafruit_ds3231.DS3231(i2c)
+
+# Čtení aktuálního času z RTC modulu
+current_time = rtc.datetime
+
+print("Aktuální čas z RTC:", current_time)
