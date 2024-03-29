4081a985ecabc6e1e2dbc1a9639c4b7d3dfff43f
ToPr <prajza.t@gmail.com>
2024-03-29 17:41:01 +0100

    Update test_casu.py

--- test_casu.py
+++ test_casu.py
@@ -12,8 +12,8 @@ rtc = adafruit_ds3231.DS3231(i2c)
 current_time = rtc.datetime
 
 print("Aktuální čas z RTC:")
-print("Rok: {:04d}".format(current_time.year))
-print("Měsíc: {:02d}".format(current_time.month))
-print("Den: {:02d}".format(current_time.day))
-print("Hodina: {:02d}".format(current_time.hour))
-print("Minuta: {:02d}".format(current_time.minute))
+print("Rok: {:04d}".format(current_time.tm_year))
+print("Měsíc: {:02d}".format(current_time.tm_mon))
+print("Den: {:02d}".format(current_time.tm_mday))
+print("Hodina: {:02d}".format(current_time.tm_hour))
+print("Minuta: {:02d}".format(current_time.tm_min))
