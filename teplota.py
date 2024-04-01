ad6b9b6d3870c1581c2010e8f6fd30b93f4be0d1
ToPr <prajza.t@gmail.com>
2024-04-01 19:27:11 +0200

    Update teplota.py

--- teplota.py
+++ teplota.py
@@ -7,11 +7,13 @@ pin = 4  # GPIO pin 4 (vyberte si správný pin, na kterém je senzor připojen)
 # Čtení dat ze senzoru
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
+humidity2, temperature2 = Adafruit_DHT.read(sensor, pin)
+
 # Kontrola, zda byla data úspěšně přečtena
 if humidity is not None and temperature is not None:
     print('Teplota: {0:0.1f} °C'.format(temperature))
     print('Vlhkost: {0:0.1f} %'.format(humidity))
-    print("Surová hodnota teploty:", temperature)
-    print("Surová hodnota vlhkosti:", humidity)
+    print("Surová hodnota teploty:", temperature2)
+    print("Surová hodnota vlhkosti:", humidity2)
 else:
     print('Chyba při čtení dat ze senzoru DHT11. Zkontrolujte připojení senzoru a zkuste to znovu.')
