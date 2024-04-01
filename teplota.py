f5058d8eb08f8f24fbf2b9130dc04e142d8bc5f5
ToPr <prajza.t@gmail.com>
2024-04-01 17:29:06 +0200

    Update teplota.py

--- teplota.py
+++ teplota.py
@@ -1,10 +1,11 @@
 from w1thermsensor import W1ThermSensor
 
 # Definice čísla GPIO pinu, na kterém je čidlo připojeno
-pin = 21  # Například GPIO pin č. 4
+pin = 4  # Například GPIO pin č. 4
 
 # Inicializace senzoru s definovaným pinem
-sensor = W1ThermSensor(GPIO=pin)
+# sensor = W1ThermSensor(GPIO=pin)
+sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, pin)
 
 # Čtení teploty
 temperature = sensor.get_temperature()
