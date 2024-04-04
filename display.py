28b3a2513677b0762a04fbf4c265a39da7cac9de
ToPr <prajza.t@gmail.com>
2024-04-05 00:17:08 +0200

    Update display.py

--- display.py
+++ display.py
@@ -1,9 +1,15 @@
-import i2clcd
+from i2clcd import I2cLcd
+import board
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
 
 
-lcd = i2clcd.i2clcd(i2c_bus=1, i2c_addr=0x3f, lcd_width=20)
+#lcd = i2clcd.i2clcd(i2c_bus=1, i2c_addr=0x3f, lcd_width=20)
+lcd = I2cLcd(i2c, 0x3f, 4, 20) 
 lcd.init()
 
-lcd.print_line('ahoj', line=0)
-lcd.print_line('tome ;)', line=1)
+lcd.clear()
 
+# Zobraz text na displeji
+lcd.move_to(0, 0)
+lcd.putstr("Hello, world!")
