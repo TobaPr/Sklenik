e9edc6eab9c7a989d464f01efc0541fdd33eda4c
ToPr <prajza.t@gmail.com>
2024-04-04 23:59:03 +0200

    Update display.py

--- display.py
+++ display.py
@@ -7,15 +7,11 @@ from adafruit_character_lcd import character_lcd_i2c
 # Inicializace I2C sběrnice
 i2c = busio.I2C(board.SCL, board.SDA)
 
-# Definice velikosti displeje (20 sloupců a 4 řádky)
-lcd_columns = 20
-lcd_rows = 4
-
-# Inicializace I2C sběrnice
 
 
 # Inicializace objektu LCD s I2C adresou 0x27
-lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns, lcd_rows, address=0x3f)
+#lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns, lcd_rows, address=0x3f)
+lcd = character_lcd_i2c.Character_LCD_I2C(i2c, 20, 4, address=0x3f)
 
 # Zapnutí LCD displeje
 lcd.backlight = True
