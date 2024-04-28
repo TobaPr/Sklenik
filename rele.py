7a3ae9ce7ccd37ebb6a0a7afc09d9299d6f0513b
ToPr <prajza.t@gmail.com>
2024-04-28 19:19:59 +0200

    Update rele.py
    Test nového relé modulu

--- rele.py
+++ rele.py
@@ -2,7 +2,12 @@ import RPi.GPIO as GPIO
 import time
 
 # Definujeme pin pro ovládání relé
-relay_pin = 19
+door_open_pin = 5
+door_close_pin = 6
+win_open_pin = 13
+win_close_pin = 19
+ventil_pin = 16
+fan_pin = 20
 
 # Nastavíme režim pinu jako výstup
 GPIO.setmode(GPIO.BCM)
@@ -10,15 +15,42 @@ GPIO.setup(relay_pin, GPIO.OUT)
 
 try:
     while True:
-        # Zapneme relé (nastavíme pin na HIGH)
-        GPIO.output(relay_pin, GPIO.HIGH)
-        print("Relay ON")
-        time.sleep(20)  # Počkáme 5 sekund
+        # Dveře
+        GPIO.output(door_close_pin, GPIO.LOW )
+        GPIO.output(door_open_pin, GPIO.HIGH)
+        print("Door open")
+        time.sleep(5)  # Počkáme 5 sekund
 
-        # Vypneme relé (nastavíme pin na LOW)
-        GPIO.output(relay_pin, GPIO.LOW)
-        print("Relay OFF")
-        time.sleep(20)  # Počkáme 5 sekund
+        GPIO.output(door_open_pin, GPIO.LOW)
+        GPIO.output(door_close_pin, GPIO.HIGH)
+        print("Door close")
+        time.sleep(5)  # Počkáme 5 sekund
+
+        # Okno
+        GPIO.output(win_close_pin, GPIO.LOW )
+        GPIO.output(win_open_pin, GPIO.HIGH)
+        print("Window open")
+        time.sleep(5)  # Počkáme 5 sekund
+        
+        GPIO.output(win_open_pin, GPIO.LOW)
+        GPIO.output(win_close_pin, GPIO.HIGH)
+        print("Window close")
+        time.sleep(5)  # Počkáme 5 sekund
+
+        # Fan
+        GPIO.output(fan_pin, GPIO.HIGH)
+        print("Fan open")
+        time.sleep(5) 
+        GPIO.output(fan_pin, GPIO.LOW)
+        print("Fan close")
+
+        #ventil
+        GPIO.output(ventil_pin, GPIO.HIGH)
+        print("Ventil open")
+        time.sleep(5) 
+        GPIO.output(ventil_pin, GPIO.LOW)
+        print("Ventil close")
+        
 
 except KeyboardInterrupt:
     # Ukončení programu při stisknutí Ctrl+C
