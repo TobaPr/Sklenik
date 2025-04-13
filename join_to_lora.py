d3f4e400f39e5b77430a6aa7336632bcafd4fde8
ToPr <prajza.t@gmail.com>
2025-04-13 17:57:13 +0200

    další pokus

--- join_to_lora.py
+++ join_to_lora.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 # V3.0.x firmware
-from rak811.rak811_v3 import Rak811
+from rak811.rak811_v3 import Mode, Rak811
 
 # test xxx
 lora = Rak811()
 lora.hard_reset()
-lora.set_config('lora:work_mode:0')
-lora.set_config('lora:join_mode:0')
-lora.set_config('lora:region:EU868')
-lora.set_config('lora:app_eui:AC1F09FFF8680811')
-lora.set_config('lora:app_key:AC1F09FFFE03DD04AC1F09FFF8680811')
-lora.join()
-lora.set_config('lora:dr:5')
+#lora.set_config('lora:work_mode:0')
+#lora.set_config('lora:join_mode:0')
+#lora.set_config('lora:region:EU868')
+#lora.set_config('lora:app_eui:AC1F09FFF8680811')
+#lora.set_config('lora:app_key:AC1F09FFFE03DD04AC1F09FFF8680811')
+#lora.join()
+#lora.set_config('lora:dr:5')
+#lora.send('Hello world')
+
+lora.mode = Mode.LoRaWan
+lora.band = 'EU868'
+lora.set_config(app_eui='AC1F09FFF8680811',
+                app_key='AC1F09FFFE03DD04AC1F09FFF8680811')
+lora.join_otaa()
+lora.dr = 5
 lora.send('Hello world')
+
+
+
 lora.close()
