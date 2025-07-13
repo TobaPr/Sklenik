2d0b21be6f5de93303c13fa5d1ad14a005424bd3
ToPr <prajza.t@gmail.com>
2025-07-13 11:11:36 +0200

    Nastavení online mode

--- Final.py
+++ Final.py
@@ -18,6 +18,8 @@ WinMovingTime = 30 # doba po kterou se pohybuje motor u okna
 VentilMovingTime = 10 # doba po kterou se pohybuje ventil
 FanDelay = 5
 
+# Příznak zda odesíláme data
+onlinemode = False
 
 
 # Nastavení pinů pro ovládání tlačítek
@@ -48,30 +50,32 @@ GPIO.setup(ventil_pin, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(fan_pin, GPIO.OUT, initial=GPIO.HIGH)
 
 def JoinToLora():
-    try:
-        lora = Rak811()
-        lora.set_config('lora:work_mode:0')
-        lora.set_config('lora:join_mode:0')
-        lora.set_config('lora:region:EU868')
-        lora.set_config('lora:app_eui:AC1F09FFF8680811')
-        lora.set_config('lora:app_key:AC1F09FFFE03DD04AC1F09FFF8680811')
-        lora.join()
-        lora.set_config('lora:dr:5')
-        print("připojuji se")
-        lora.send('Start',100)
-        lora.close()
-        print("Pripojeno")
-    except Exception as e:
-        print("Došlo k chybě při připojování k LoRa:", e)
+    if onlinemode:
+        try:
+            lora = Rak811()
+            lora.set_config('lora:work_mode:0')
+            lora.set_config('lora:join_mode:0')
+            lora.set_config('lora:region:EU868')
+            lora.set_config('lora:app_eui:AC1F09FFF8680811')
+            lora.set_config('lora:app_key:AC1F09FFFE03DD04AC1F09FFF8680811')
+            lora.join()
+            lora.set_config('lora:dr:5')
+            print("připojuji se")
+            lora.send('Start',100)
+            lora.close()
+            print("Pripojeno")
+        except Exception as e:
+            print("Došlo k chybě při připojování k LoRa:", e)
 
 def SendLoraMesagge(text,port):
-    try:
-        print("Posilam zpravu: " + str(text) + ' '+ str(port))
-        lora = Rak811()
-        lora.send(text,int(port))
-        lora.close()
-    except Exception as e:
-        print("Došlo k chybě při odesílání zprávy:", e)
+    if onlinemode:
+        try:
+            print("Posilam zpravu: " + str(text) + ' '+ str(port))
+            lora = Rak811()
+            lora.send(text,int(port))
+            lora.close()
+        except Exception as e:
+            print("Došlo k chybě při odesílání zprávy:", e)
     
         
 def GetRTCTime():
